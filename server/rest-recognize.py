86cfd248704208732185e2821e077ac06b70efee
Kien Luong <giakienlgk@gmail.com>
2019-05-22 17:15:30 +0700

    add demo client

--- server/rest-recognize.py
+++ server/rest-recognize.py
@@ -33,6 +33,7 @@ from lib.src.align import detect_face
 from tensorflow.python.platform import gfile
 from lib.src.facenet import get_model_filenames
 from lib.src.recognize import recognize_face_stream
+from lib.src.recognize import get_frame
 
 from dict2obj import to_obj
 
@@ -41,7 +42,7 @@ app = Flask(__name__, static_url_path="")
 auth = HTTPBasicAuth()
 
 config_args = {"detect_multiple_faces": True, "margin": 44, "image_size": 160,
-               "embedding_dir": "~/workspace/hdd/Kien/Face_Recognition/lib/src/embedding_dict.pickle",
+               "embedding_dir": "~/workspace/hdd/Kien/Face_Recognition/lib/src/embedding_ktx.pickle",
                "ckpt": "~/workspace/hdd/Kien/Face_Recognition/lib/src/ckpt/20180402-114759"}
 
 # ==============================================================================================================================
@@ -71,7 +72,6 @@ with graph_fr.as_default():
     saverf.restore(sess_fr, os.path.join(ckpt, ckpt_file))
     pnet, rnet, onet = detect_face.create_mtcnn(sess_fr, None)
 
-
 # ==============================================================================================================================
 #
 
@@ -81,14 +81,18 @@ with graph_fr.as_default():
 #
 
 # ==============================================================================================================================
+# recognize_face_stream(sess_fr, pnet, rnet, onet, feature_array, to_obj(config_args))
+
+
 @app.route('/facerecognitionLive', methods=['GET', 'POST'])
 def face_det():
-    recognize_face_stream(sess_fr, pnet, rnet, onet, feature_array, to_obj(config_args))
+    # recognize_face_stream(sess_fr, pnet, rnet, onet, feature_array, to_obj(config_args))
+    return get_frame()
+#
+# @app.route('/getLatestFrame', method=['GET', 'POST'])
+# def get_latest_frame():
+#     return get_frame()
 
-
-@app.route('/getLatestFrame', method=['GET', 'POST'])
-def get_latest_frame():
-    return []
 
 # ==============================================================================================================================
 #
@@ -103,4 +107,4 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port='8888')
